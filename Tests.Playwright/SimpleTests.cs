c5ebe02c2892fe7a600d5c25bf83b0f258d58b9e
Marcel de Vries <vriesmarcel@hotmail.com>
2025-04-02 10:45:23 +0200

    fix prep

--- Tests.Playwright/SimpleTests.cs
+++ Tests.Playwright/SimpleTests.cs
@@ -1,9 +1,10 @@
+using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 using Tests.Playwright.HelperClasses;
 namespace Tests.Playwright
 {
     [TestFixture] 
-    public class SimpleTests : PlaywrightTestWithArtifact
+    public class SimpleTests : PageTest
     {
         public string StartPage = "https://globoticket-frontend-dpfbe7hxa6d2bdab.westeurope-01.azurewebsites.net/";
 
